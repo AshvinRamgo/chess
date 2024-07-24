d9ad31fede04262ec2fa6f0a1da0a601c7fd9513
haowenhoward <129302330+haowenhoward@users.noreply.github.com>
2024-07-24 11:44:42 -0400

    Add files via upload

---
+++ text_display.cc
@@ -0,0 +1,55 @@
+#include "text_display.h"
+Text_Display::Text_Display(const Board& b): board{b} {}
+void Text_Display::render() {
+    std::cout << WRAPPER;
+    for (int i = 0; i < 10; i++) {
+        std::cout << '-';
+    }
+    std::cout << '\n';
+    for (int row = 8; row > 0; row--) {
+        std::cout << '|';
+        for (int column = 0; column < 8; column++) { 
+            std::string pos = "";
+            pos += ('a' + column);
+            pos += ('0' + row);
+            char piece = board.view(pos);
+            if (piece == ' ') {
+                if (((column % 2) + (row % 2)) % 2 == 0) {
+                    piece = '#';
+                } else {
+                    piece = ' ';
+                }
+            }
+            std::cout << piece;
+        }
+        std::cout << "|\n";
+    }
+    for (int i = 0; i < 10; i++) {
+        std::cout << '-';
+    }
+    std::cout << '\n' << WRAPPER << '\n';
+}
+
+void Text_Display::score(int black, int white) {
+    std::cout << WRAPPER << "Current Score: \n" << "Black: " << black << ", White: " << white << std::endl << WRAPPER << '\n';
+}
+
+void Text_Display::resign(char color) {
+    if (color == 'w') {
+        std::cout << WRAPPER << WHITE_RESIGN << WRAPPER << '\n';
+    } else {
+        std::cout << WRAPPER << BLACK_RESIGN << WRAPPER << '\n';
+    }
+}
+void Text_Display::checkmate(char color) {
+    if (color == 'w') {
+        std::cout << WRAPPER << WHITE_CHECKMATE << WRAPPER << '\n';
+    } else {
+        std::cout << WRAPPER << BLACK_CHECKMATE << WRAPPER << '\n';
+    }
+}
+
+void Text_Display::stalemate() {
+    std::cout << WRAPPER << STALEMATE << WRAPPER << '\n';
+}
+
---
+++ text_display.h
@@ -0,0 +1,19 @@
+#include "board.h"
+#include "iostream"
+class Text_Display {
+    const Board& board;
+    const std::string WRAPPER = "----------------------------------------\n";
+    const std::string WHITE_RESIGN = "Black wins! By Resignation\n";
+    const std::string BLACK_RESIGN = "White wins! By Resignation\n";
+    const std::string WHITE_CHECKMATE = "White wins! By Checkmate\n";
+    const std::string BLACK_CHECKMATE = "Black wins! By Checkmate\n";
+    const std::string STALEMATE = "Game tied! By Stalemate\n";
+    public:
+        Text_Display(const Board& b);
+        void render();
+        void score(int black, int white);
+        void resign(char color);
+        void checkmate(char color);
+        void stalemate();
+
+};
